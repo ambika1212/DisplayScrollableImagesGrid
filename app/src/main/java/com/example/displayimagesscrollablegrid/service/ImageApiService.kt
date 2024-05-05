36f604bf0bfe133ef681f7596ed45ceeb55260b1
Ambika <ambikabiradar402@gmail.com>
2024-05-05 10:21:16 +0530

    git commit message example

---
+++ app/src/main/java/com/example/displayimagesscrollablegrid/service/ImageApiService.kt
@@ -0,0 +1,9 @@
+package com.example.displayimagesscrollablegrid.service
+
+import retrofit2.http.GET
+
+interface ImageApiService {
+    @GET("/photos/random?count=30&client_id=d1E5Y9RLxdQ33qSIwJTfiShgZyXhexRmVR2UO2ljvo4")
+    suspend fun getImages(): List<String>
+
+}
